4bd9e56b7a6281a4e5eafa3248409882e7520325
balasm <01142551@pw.edu.pl>
2020-10-25 02:02:21 +0200

    first look of register page, inputs yet to be done

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def render_main():
     return render_template("index.html")
 
 
+@app.route('/sender/register')
+def render_sign_up():
+    return render_template("register.html")
+
 
 if __name__ == '__main__':
     app.run()
